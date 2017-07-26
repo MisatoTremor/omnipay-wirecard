34d242e541e9ac528d83d82b7eeb8fb9ed22c67e
Steffen Roßkamp <steffen.rosskamp@gimmickmedia.de>
2017-07-26 13:04:24 +0200

    Add UUID4 generation for requests

--- src/Message/Builder/AbstractBuilder.php
+++ src/Message/Builder/AbstractBuilder.php
@@ -32,7 +32,7 @@ abstract class AbstractBuilder implements BuilderInterface
 
         $payment = new Payment();
         $payment->merchantAccountId = $merchantAccountId;
-        $payment->requestId = $this->request->getTransactionId();
+        $payment->requestId = $this->uuid4();
         $payment->parentTransactionId = $this->request->getTransactionReference();
         $payment->transactionType = $this->getTransactionType();
         $payment->notifications = [$notification];
@@ -44,4 +44,19 @@ abstract class AbstractBuilder implements BuilderInterface
      * @return string
      */
     abstract protected function getTransactionType();
+
+    /**
+     * @link https://stackoverflow.com/a/15875555/7774851 By courtesy of Jack
+     *
+     * @return string
+     */
+    protected function uuid4()
+    {
+        $data = random_bytes(16);
+        assert(strlen($data) == 16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
 }
